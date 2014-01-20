1f476cd29b9b7fbb4b0614e9d910604ddf535173
TsungWei Hu <marr.tw@gmail.com>
2014-01-20 10:28:52 +0800

    Add deity_main and founded Indexer for Temple Type.

--- crgis/policy/indexers.py
+++ crgis/policy/indexers.py
@@ -1,6 +1,27 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
 from plone.indexer.decorator import indexer
+from crgis.atcontents.interfaces.temple import ITemple
 from crgis.content.theater import ITheater
 
+
+@indexer(ITemple)
+def deity_main(object, **kw):
+    results = []
+    factory = getUtility(IVocabularyFactory, 'deity_name')
+    vocabulary = factory(object)
+    for value in object.getDeity_host():
+        try:
+            existing = vocabulary.getTerm(value)
+            results.append(existing.title.split('，')[0])
+        except LookupError:
+            results.append(value)
+    return results
+
+@indexer(ITemple)
+def founded(obj):
+    return obj.era
 
 @indexer(ITheater)
 def adm_area(obj):
