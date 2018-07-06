6e925fcef1ababf5661b2ea08e485acea4fcdd55
TsungWei Hu <marr.tw@gmail.com>
2018-07-06 10:49:07 +0800

    Update Index for Buddhist Type

--- crgis/policy/indexers.py
+++ crgis/policy/indexers.py
@@ -4,36 +4,112 @@
 from zope.component import getUtility
 from zope.schema.interfaces import IVocabularyFactory
 from plone.indexer.decorator import indexer
+from BeautifulSoup import BeautifulSoup as bs
+from Products.ATContentTypes.interfaces import IATDocument
 from crgis.atcontents.interfaces.temple import ITemple
-from crgis.content.theater import ITheater
-from crgis.content.liuyu import ILiuYu
+#from crgis.content.theater import ITheater
+#from crgis.content.liuyu import ILiuYu
 from crgis.atcontents.interfaces import IBiXieWu
 
 
+#@indexer(IATDocument)
+#def portal_type(obj):
+#    if obj.__parent__.getId() == 'bgis':
+#        return 'Buddhist'
+
+@indexer(IATDocument)
+def bgis_type(obj):
+    if obj.__parent__.getId() == 'bgis':
+        try:
+            k, v = bs(obj.getText()).find('p', {'class': 'ct'}).text.split(': ')
+            if k == u'分類': return v
+        except ValueError:
+            pass
+
+@indexer(IATDocument)
+def area1_bgis(obj):
+    if obj.__parent__.getId() == 'bgis':
+        k, v = bs(obj.getText()).find('p', {'class': 'pr'}).text.split(': ')
+        if k == u'省': return v
+
 @indexer(ITemple)
-def deity_main(object, **kw):
+def deity_main(obj, **kw):
     results = []
     factory = getUtility(IVocabularyFactory, 'deity_name')
-    vocabulary = factory(object)
-    for value in object.getDeity_host():
+    vocabulary = factory(obj)
+    for value in obj.getDeity_host():
         try:
             existing = vocabulary.getTerm(value)
-            results.append(existing.title.split(u'\uff0c')[0])
+            results.append(existing.title.split(u': ')[0].encode('utf8'))
         except LookupError:
             results.append(value)
     return results
 
 @indexer(ITemple)
+def deity(obj, **kw):
+    results = []
+    factory = getUtility(IVocabularyFactory, 'deity_name')
+    vocabulary = factory(obj)
+    for value in (set(obj.getDeity_host()) | set(obj.getDeity_company())):
+        try:
+            existing = vocabulary.getTerm(value)
+            results.append(existing.title.split(u': ')[0].encode('utf8'))
+        except LookupError:
+            results.append(value)
+    return results
+
+@indexer(ITemple)
+def wynm(obj):
+    results = []
+    for value in obj.getWysm().split(u';'):
+        if value == u'': continue
+        results.append(u''.join(value.split(u',')))
+    return results
+
+@indexer(ITemple)
+def area1_temple(obj):
+    return obj.__parent__.__parent__.Title()
+
+@indexer(ITemple)
+def area2(obj):
+    return obj.__parent__.__parent__.Title() + obj.__parent__.Title()
+
+@indexer(ITemple)
+def hostcmpn(obj):
+    results = []
+    host = obj.getDeity_host()
+    cmpn = obj.getDeity_company()
+    if 'deity_001' in host or '\xe7\x8e\x8b\xe7\x88\xba' in host:
+        results.append('hwy')
+    if 'deity_001' in cmpn or '\xe7\x8e\x8b\xe7\x88\xba' in cmpn:
+        results.append('cwy')
+    if 'deity_005' in host or '\xe5\xa4\xa9\xe4\xb8\x8a\xe8\x81\x96\xe6\xaf\x8d' in host or '\xe5\xaa\xbd\xe7\xa5\x96' in host:
+        results.append('hmz')
+    if 'deity_005' in cmpn or '\xe5\xa4\xa9\xe4\xb8\x8a\xe8\x81\x96\xe6\xaf\x8d' in cmpn or '\xe5\xaa\xbd\xe7\xa5\x96' in cmpn:
+        results.append('cmz')
+    if 'deity_008' in host or '\xe7\x8e\x84\xe5\xa4\xa9\xe4\xb8\x8a\xe5\xb8\x9d' in host:
+        results.append('hxt')
+    if 'deity_008' in cmpn or '\xe7\x8e\x84\xe5\xa4\xa9\xe4\xb8\x8a\xe5\xb8\x9d' in cmpn:
+        results.append('cxt')
+    return results
+
+@indexer(ITemple)
 def founded(obj):
-    return obj.era
+    if obj.era == '':
+        pass
+    else:
+        try:
+            return int(obj.era)
+        except ValueError:
+            pass
 
-@indexer(ITheater)
-def adm_area(obj):
-    return obj.adm_area
+#@indexer(ITheater)
+#def adm_area(obj):
+#    return obj.adm_area
 
-@indexer(ITheater)
-def function(obj):
-    return obj.function
+#@indexer(ITheater)
+#def function(obj):
+#    return obj.function
 
 @indexer(IBiXieWu)
 def purpose(obj):
@@ -75,17 +151,17 @@ def lct_tow(obj):
 def lct_vil(obj):
     return obj.lct_vil
 
-@indexer(ITheater)
-def owner(obj):
-    return obj.owner
+#@indexer(ITheater)
+#def owner(obj):
+#    return obj.owner
 
-@indexer(ITheater)
-def operator(obj):
-    return obj.operator
+#@indexer(ITheater)
+#def operator(obj):
+#    return obj.operator
 
-@indexer(ITheater)
-def in_charge(obj):
-    return obj.in_charge
+#@indexer(ITheater)
+#def in_charge(obj):
+#    return obj.in_charge
 
 @indexer(IBiXieWu)
 def height(obj):
@@ -98,74 +174,74 @@ def height(obj):
         value = None
     return value
 
-@indexer(ILiuYu)
-def time_start(obj):
-    if obj.year_start:
-        try:
-            time = int(obj.year_start)
-        except ValueError:
-            time = None
-    else:
-        time = None
-    return time
+#@indexer(ILiuYu)
+#def time_start(obj):
+#    if obj.year_start:
+#        try:
+#            time = int(obj.year_start)
+#        except ValueError:
+#            time = None
+#    else:
+#        time = None
+#    return time
 
-@indexer(ILiuYu)
-def country(obj):
-    return obj.country
+#@indexer(ILiuYu)
+#def country(obj):
+#    return obj.country
 
-@indexer(ILiuYu)
-def state(obj):
-    return obj.state
+#@indexer(ILiuYu)
+#def state(obj):
+#    return obj.state
 
-@indexer(ILiuYu)
-def city(obj):
-    return obj.city
+#@indexer(ILiuYu)
+#def city(obj):
+#    return obj.city
 
-@indexer(ILiuYu)
-def county(obj):
-    return obj.county
+#@indexer(ILiuYu)
+#def county(obj):
+#    return obj.county
 
-@indexer(ILiuYu)
-def river(obj):
-    return obj.river
+#@indexer(ILiuYu)
+#def river(obj):
+#    return obj.river
 
-@indexer(ILiuYu)
-def mountain(obj):
-    return obj.mountain
+#@indexer(ILiuYu)
+#def mountain(obj):
+#    return obj.mountain
 
-@indexer(ILiuYu)
-def monastery(obj):
-    return obj.monastery
+#@indexer(ILiuYu)
+#def monastery(obj):
+#    return obj.monastery
 
-@indexer(ILiuYu)
-def tomb(obj):
-    return obj.tomb
+#@indexer(ILiuYu)
+#def tomb(obj):
+#    return obj.tomb
 
-@indexer(ILiuYu)
-def castle(obj):
-    return obj.castle
+#@indexer(ILiuYu)
+#def castle(obj):
+#    return obj.castle
 
-@indexer(ILiuYu)
-def fortress(obj):
-    return obj.fortress
+#@indexer(ILiuYu)
+#def fortress(obj):
+#    return obj.fortress
 
-@indexer(ILiuYu)
-def relices(obj):
-    return obj.relices
+#@indexer(ILiuYu)
+#def relices(obj):
+#    return obj.relices
 
-@indexer(ILiuYu)
-def buildings(obj):
-    return obj.buildings
+#@indexer(ILiuYu)
+#def buildings(obj):
+#    return obj.buildings
 
-@indexer(ILiuYu)
-def legend(obj):
-    return obj.legend
+#@indexer(ILiuYu)
+#def legend(obj):
+#    return obj.legend
 
-@indexer(ILiuYu)
-def people(obj):
-    return obj.people.raw
+#@indexer(ILiuYu)
+#def people(obj):
+#    return obj.people.raw
 
-@indexer(ILiuYu)
-def ref_name(obj):
-    return obj.ref_name
+#@indexer(ILiuYu)
+#def ref_name(obj):
+#    return obj.ref_name
 
